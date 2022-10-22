b63c0225a08a17225c715a589c6c580f2a0ef958
Julian <julianmuchacho666@gmail.com>
2022-10-22 16:31:18 -0400

    donated items + other

--- PluginTemplate/AvMain.cs
+++ PluginTemplate/AvMain.cs
@@ -1,4 +1,5 @@
 ﻿using AverageTerrariaMain;
+using AverageTerrariaSurvival;
 using System;
 using Terraria.ObjectData;
 using Terraria;
@@ -9,6 +10,12 @@ using System.Timers;
 using Microsoft.Xna.Framework;
 using TShockAPI.Localization;
 using Terraria.Localization;
+using System.Collections.Generic;
+using Mono.Data.Sqlite;
+using System.IO;
+using System.Text;
+using System.Data;
+using System.Linq.Expressions;
 
 namespace PluginTemplate
 {
@@ -21,7 +28,10 @@ namespace PluginTemplate
 		internal static readonly AvPlayers Players = new AvPlayers();
 
 		public Timer bcTimer;
+        private IDbConnection _db;
+        public static Database dbManager;
 
+        public static DonatedItems _donatedItems = new DonatedItems();
         /// <summary>
         /// The name of the plugin.
         /// </summary>
@@ -72,6 +82,20 @@ namespace PluginTemplate
 			TShockAPI.Hooks.RegionHooks.RegionEntered += onRegionEnter;
 			TShockAPI.Hooks.RegionHooks.RegionLeft += onRegionLeave;
 			Console.WriteLine("Average Survival LOADED");
+
+            switch (TShock.Config.Settings.StorageType.ToLower())
+            {
+                case "sqlite":
+                    _db = new SqliteConnection(string.Format("uri=file://{0},Version=3",
+                        Path.Combine(TShock.SavePath, "AvSurvival.sqlite")));
+                    break;
+                default:
+                    throw new Exception("Invalid storage type.");
+            }
+
+            DonatedItems items = new DonatedItems();
+            
+            dbManager = new Database(_db);
         }
 
 		public void broadcastMessage(Object source, ElapsedEventArgs args)
@@ -89,11 +113,147 @@ namespace PluginTemplate
             }
         }
 
-        void onPlayerDeath(object sender, GetDataHandlers.PlayerDamageEventArgs args)
+        void bossProgression(CommandArgs args)
+        {
+            Dictionary<string, Color> bossProg = new Dictionary<string, Color> { };
+            int bossesKilled = 0;
+
+            if (NPC.downedBoss1)
+            {
+                bossProg.Add("Eye Of Cthulhu - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Eye Of Cthulhu - Not Defeated", Color.Red);
+            }
+
+            if (NPC.downedBoss2)
+            {
+                bossProg.Add("Eater of Worlds - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Eater of Worlds - Not Defeated", Color.Red);
+            }
+
+            if (NPC.downedBoss3)
+            {
+                bossProg.Add("Skeletron - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Skeletron - Not Defeated", Color.Red);
+            }
+            if (Main.hardMode == true)
+            {
+                bossProg.Add("Wall of Flesh - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Wall of Flesh - Not Defeated", Color.Red);
+            }
+            if (NPC.downedMechBoss1)
+            {
+                bossProg.Add("Destroyer - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Destroyer - Not Defeated", Color.Red);
+            }
+            if (NPC.downedMechBoss2)
+            {
+                bossProg.Add("The Twins - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("The Twins - Not Defeated", Color.Red);
+            }
+            if (NPC.downedMechBoss3)
+            {
+                bossProg.Add("Skeletron Prime - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Skeletron Prime - Not Defeated", Color.Red);
+            }
+            if (NPC.downedPlantBoss)
+            {
+                bossProg.Add("Plantera - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Plantera - Not Defeated", Color.Red);
+            }
+            if (NPC.downedGolemBoss)
+            {
+                bossProg.Add("Golem - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Golem - Not Defeated", Color.Red);
+            }
+            if (NPC.downedFishron)
+            {
+                bossProg.Add("Duke Fishron - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Duke Fishron - Not Defeated", Color.Red);
+            }
+            if (NPC.downedEmpressOfLight)
+            {
+                bossProg.Add("Empress of Light - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Empress of Light - Not Defeated", Color.Red);
+            }
+            if (NPC.downedAncientCultist)
+            {
+                bossProg.Add("Lunatic Cultist - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Lunatic Cultist - Not Defeated", Color.Red);
+            }
+            if (NPC.downedMoonlord)
+            {
+                bossProg.Add("Moon Lord - Defeated", Color.Green);
+                bossesKilled++;
+            }
+            else
+            {
+                bossProg.Add("Moon Lord - Not Defeated", Color.Red);
+            }
+
+            if(bossesKilled <= 0)
+            {
+                args.Player.SendMessage("No bosses have been defeated yet!", Color.Red);
+            }
+
+            foreach(KeyValuePair<string,Color> boss in bossProg)
+            {
+                args.Player.SendMessage(boss.Key, boss.Value);
+            }
+        }
+
+            void onPlayerDeath(object sender, GetDataHandlers.PlayerDamageEventArgs args)
         {
             TSPlayer player = TSPlayer.FindByNameOrID(args.PlayerDeathReason._sourcePlayerIndex.ToString())[0];
 
-            if(Players.GetByUsername(player.Name).isBountied == true)
+            if(Players.GetByUsername(player.Name).isBountied == true && Main.player[player.Index].statLife <= 0)
             {
                 TimeRanks.TimeRanks.Players.GetByUsername(player.Name).totalCurrency += Players.GetByUsername(player.Name).bountyPrice;
                 Players.GetByUsername(player.Name).isBountied = false;
@@ -101,6 +261,165 @@ namespace PluginTemplate
             }
             
         }
+
+        private List<int> ItemList
+        {
+            get
+            {
+                List<int> list = new List<int>();
+                var items = typeof(Terraria.ID.ItemID).GetFields();
+                for (int i = 0; i < items.Length; i++)
+                {
+                    list.Add((int)items[i].GetValue(items[i]));
+                }
+                list.Sort();
+                return list;
+            }
+        }
+
+        #region GenChests Command
+        private void DoChests(CommandArgs args)
+        {
+            if (args.Parameters.Count == 0 || args.Parameters.Count > 2)
+            {
+                args.Player.SendInfoMessage("Usage: /genchests <amount> [gen mode: default/easy/all]");
+            }
+            int empty = 0;
+            int tmpEmpty = 0;
+            int chests = 0;
+            int maxChests = 1000;
+
+            string setting = "default";
+            if (args.Parameters.Count > 1)
+            {
+                setting = args.Parameters[1];
+            }
+            const int maxtries = 100000;
+            Int32.TryParse(args.Parameters[0], out chests);
+            const int threshold = 100;
+                for (int x = 0; x < maxChests; x++)
+                {
+                    if (Main.chest[x] != null)
+                    {
+                        tmpEmpty++;
+                        bool found = false;
+                        foreach (Item itm in Main.chest[x].item)
+                            if (itm.netID != 0)
+                                found = true;
+                        if (found == false)
+                        {
+                            empty++;
+                            //      TShock.Utils.Broadcast(string.Format("Found chest {0} empty at x {1} y {2}", x, Main.chest[x].x,
+                            //                                           Main.chest[x].y));
+
+                            // destroying
+                            WorldGen.KillTile(Main.chest[x].x, Main.chest[x].y, false, false, false);
+                            Main.chest[x] = null;
+
+                        }
+
+                    }
+
+                }
+                args.Player.SendSuccessMessage("Uprooted {0} empty out of {1} chests.", empty, tmpEmpty);
+            
+            if (chests + tmpEmpty + threshold > maxChests)
+                chests = maxChests - tmpEmpty - threshold;
+            if (chests > 0)
+            {
+                int chestcount = 0;
+                chestcount = tmpEmpty;
+                int tries = 0;
+                int newcount = 0;
+                while (newcount < chests)
+                {
+                    int contain;
+                    if (setting == "default")
+                    {
+                        // Moved item list into a separate .txt file
+                        int[] itemID = Config.DefaultChestIDs;
+                        contain = itemID[WorldGen.genRand.Next(0, itemID.Length)];
+                    }
+                    else if (setting == "all")
+                    {
+                        // Updated item list to 1.2.4.1
+                        contain = WorldGen.genRand.Next(ItemList[0], ItemList.Count + 1);
+                    }
+                    else if (setting == "easy")
+                    {
+                        contain = WorldGen.genRand.Next(-24, 364);
+                    }
+                    else
+                    {
+                        args.Player.SendWarningMessage("Warning! Typo in second argument: {0}", args.Parameters[1]);
+                        return;
+                    }
+                    int tryX = WorldGen.genRand.Next(20, Main.maxTilesX - 20);
+                    int tryY = WorldGen.genRand.Next((int)Main.worldSurface, Main.maxTilesY - 200);
+                    while (!Main.tile[tryX, tryY].active())
+                    {
+                        tryY++;
+                    }
+                    tryY--;
+                    WorldGen.KillTile(tryX, tryY, false, false, false);
+                    WorldGen.KillTile(tryX + 1, tryY, false, false, false);
+                    WorldGen.KillTile(tryX, tryY + 1, false, false, false);
+                    WorldGen.KillTile(tryX + 1, tryY, false, false, false);
+
+                    if (WorldGen.AddBuriedChest(tryX, tryY, contain, true, 1))
+                    {
+                        chestcount++;
+                        newcount++;
+
+                            StringBuilder items = new StringBuilder();
+                            Terraria.Chest c = Main.chest[0];
+                            if (c != null)
+                            {
+                                for (int j = 0; j < 40; j++)
+                                {
+                                    items.Append(c.item[j].netID + "," + c.item[j].stack + "," + c.item[j].prefix);
+                                    if (j != 39)
+                                    {
+                                        items.Append(",");
+                                    }
+                                }
+                                items.Clear();
+                                Main.chest[0] = null;
+                            }
+                        
+
+                    }
+                    if (tries + 1 >= maxtries)
+                        break;
+
+                    tries++;
+                }
+                args.Player.SendSuccessMessage("Generated {0} new chests - {1} total", newcount, chestcount);
+                InformPlayers();
+            }
+        }
+        #endregion
+
+        public static void InformPlayers(bool hard = false)
+		{
+			foreach (TSPlayer person in TShock.Players)
+			{
+				if ((person != null) && (person.Active))
+				{
+					for (int i = 0; i < 255; i++)
+					{
+						for (int j = 0; j < Main.maxSectionsX; j++)
+						{
+							for (int k = 0; k < Main.maxSectionsY; k++)
+							{
+                                NetMessage.SendSection(i, j, k);
+							}
+						}
+					}
+				}
+			}
+
+		}
 
         void onBossSpawn(NpcSpawnEventArgs args)
         {
@@ -110,12 +429,26 @@ namespace PluginTemplate
             {
                 
                 TSPlayer.All.SendMessage("A suspicious eye is upon us!", Color.Red);
-                npc.lifeMax = 10000;
-                npc.life = 10000;
+                npc.lifeMax = 10000+(TShock.Players.Length*2000);
+                npc.life = 10000+(TShock.Players.Length * 2000);
                 npc.height = 100;
                 npc.width = 100;
-                npc.GivenName = "Suspicious Eye";
                 NetMessage.SendData(23, -1, -1, (NetworkText) null, args.NpcId);
+                TSPlayer.Server.StrikeNPC(args.NpcId, 1, 0, 0);
+                TSPlayer.All.SendData(PacketTypes.NpcUpdate, "", args.NpcId);
+                TSPlayer.All.SendData(PacketTypes.UpdateNPCName, npc.GivenName);
+                //update on client side immediately
+                args.Handled = true;
+            }
+            if (npc.netID == NPCID.SkeletronHead)
+            {
+
+                TSPlayer.All.SendMessage("A suspicious eye is upon us!", Color.Red);
+                npc.lifeMax = 20000 + (TShock.Players.Length * 2000);
+                npc.life = 20000 + (TShock.Players.Length * 2000);
+                npc.height = 100;
+                npc.width = 100;
+                NetMessage.SendData(23, -1, -1, (NetworkText)null, args.NpcId);
                 TSPlayer.Server.StrikeNPC(args.NpcId, 1, 0, 0);
                 TSPlayer.All.SendData(PacketTypes.NpcUpdate, "", args.NpcId);
                 TSPlayer.All.SendData(PacketTypes.UpdateNPCName, npc.GivenName);
@@ -143,14 +476,82 @@ namespace PluginTemplate
 			Commands.ChatCommands.Add(new Command("av.discord", discordInvite, "discord"));
             Commands.ChatCommands.Add(new Command("av.reload", reloadCommand, "avreload"));
             Commands.ChatCommands.Add(new Command("av.bounty", Bounty, "bounty"));
-
+            Commands.ChatCommands.Add(new Command("av.bounty", bossProgression, "bosses", "progression", "far"));
+            Commands.ChatCommands.Add(new Command("av.chests", DoChests, "chests", "genChests"));
+            Commands.ChatCommands.Add(new Command("av.donate", Donate, "donate", "ditem"));
+            Commands.ChatCommands.Add(new Command("av.receive", ReceiveDonation, "beg", "receive", "plz"));
 
             bcTimer = new Timer(Config.bcInterval*1000*60); //minutes
 
 			bcTimer.Elapsed += broadcastMessage;
 			bcTimer.AutoReset = true;
 			bcTimer.Enabled = true;
-		}
+
+            dbManager.InitialSyncPlayers();
+
+
+        }
+        void Donate(CommandArgs args)
+        {
+            Item item = args.Player.SelectedItem;
+            if(item == null)
+            {
+                args.Player.SendMessage("You must be holding an item!", Color.IndianRed);
+                return;
+            }
+            _donatedItems.donations.Add(new DonatedItem(item.netID, item.stack, item.prefix));
+            var e = 0;
+
+            foreach(Item i in Main.player[args.Player.Index].inventory)
+            {
+                if(i == item)
+                {
+                    break;
+                }
+                e++;
+            }
+            args.Player.TPlayer.inventory[e] = new Item();
+            NetMessage.SendData((int)PacketTypes.PlayerSlot, -1, -1, new NetworkText(Main.player[args.Player.Index].inventory[e].Name, NetworkText.Mode.Literal), args.Player.Index, e, 0);
+            NetMessage.SendData((int)PacketTypes.PlayerSlot, args.Player.Index, -1, new NetworkText(Main.player[args.Player.Index].inventory[e].Name, NetworkText.Mode.Literal), args.Player.Index, e, 0);
+
+            var dolla = item.GetStoreValue() * item.stack / 5000;
+
+            if(dolla == 0)
+            {
+                dolla++;
+            }
+
+            TimeRanks.TimeRanks.Players.GetByUsername(args.Player.Name).totalCurrency += dolla;
+            args.Player.SendMessage("You have inserted a " + item.Name + " into the donation pool and received " + dolla + " dollas!", Color.LightGreen);
+            dbManager.InsertItem(new DonatedItem(item.netID, item.stack, item.prefix));
+        }
+
+        void ReceiveDonation(CommandArgs args)
+        {
+            if(_donatedItems.donations.Count <= 0)
+            {
+                args.Player.SendMessage("There are currently no items in the donation pool! Use /donate to insert an item!", Color.IndianRed);
+                return;
+            }
+
+            if (args.Player.InventorySlotAvailable)
+            {
+                Random r = new Random();
+                var item = _donatedItems.donations[r.Next(0, _donatedItems.donations.Count)];
+
+                args.Player.GiveItem(item.id, item.quantity, item.prefix);
+                dbManager.DeleteItem(item);
+                _donatedItems.donations.Remove(item);
+                args.Player.SendMessage("You have received a " + EnglishLanguage.GetItemNameById(item.id) + " from the donation pool!", Color.LightGreen);
+            }
+            else
+            {
+                args.Player.SendMessage("You do not have any available inventory slots!", Color.IndianRed);
+                return;
+            }
+
+        }
+
 
         void Bounty(CommandArgs b)
         {
@@ -327,6 +728,24 @@ namespace PluginTemplate
 
         void onTileEdit(object sender, GetDataHandlers.TileEditEventArgs tile)
         {
+            if(tile.Action == GetDataHandlers.EditAction.KillTile && Main.tile[tile.X, tile.Y].type == 21)
+            {
+                var chest = Main.chest[Chest.FindChest(tile.X, tile.Y)];
+
+                    foreach(Item item in chest.item)
+                    {
+                        if (tile.Player.InventorySlotAvailable == true)
+                        {
+                            tile.Player.GiveItem(item.netID, item.stack, item.prefix);
+                            item.active = false;
+                            item.UpdateItem(0);
+                            NetMessage.SendData((int)PacketTypes.ChestGetContents, -1, -1, null, item.netID);
+                        }
+                    }
+                
+            }
+
+
             if(tile.Action == GetDataHandlers.EditAction.KillTile && tile.EditData == 0)
             {
                 //Copper behaviour
---
+++ PluginTemplate/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using MySql.Data.MySqlClient;
+using TShockAPI.DB;
+
+namespace AverageTerrariaSurvival
+{
+    public class Database
+    {
+        private readonly IDbConnection _db;
+
+        public Database(IDbConnection db)
+        {
+            _db = db;
+
+            var sqlCreator = new SqlTableCreator(db, db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+
+            var table = new SqlTable("DonatedItems",
+                new SqlColumn("Id", MySqlDbType.Int32) { Primary = true, AutoIncrement = true },
+                new SqlColumn("ItemID", MySqlDbType.Int32),
+                new SqlColumn("Quantity", MySqlDbType.Int32, 50),
+                new SqlColumn("Prefix", MySqlDbType.Int32)
+                );
+            sqlCreator.EnsureTableStructure(table);
+        }
+        public bool InsertItem(DonatedItem item)
+        {
+            return _db.Query("INSERT INTO DonatedItems (ItemID, Quantity, Prefix)" + "VALUES (@0, @1, @2)", item.id, item.quantity, item.prefix) != 0;
+        }
+
+        public bool DeleteItem(DonatedItem item)
+        {
+            return _db.Query("DELETE FROM DonatedItems WHERE Id = @0", item.dbId) != 0;
+        }
+
+
+        public void InitialSyncPlayers()
+        {
+            using (var reader = _db.QueryReader("SELECT * FROM DonatedItems"))
+            {
+                while (reader.Read())
+                {
+                    var actualId = reader.Get<int>("Id");
+                    var itemID = reader.Get<int>("ItemID");
+                    var Quantity = reader.Get<int>("Quantity");
+                    var prefix = reader.Get<int>("Prefix");
+
+
+                    PluginTemplate.AvMain._donatedItems.donations.Add(new DonatedItem(actualId, itemID, Quantity, prefix));
+
+                    
+                }
+            }
+        }
+    }
+}
